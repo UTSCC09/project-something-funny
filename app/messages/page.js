46ad1de131cf20ca97b62d8326f70f74f88984b9
Masahisa Sekita <masahisa.sekita@mail.utoronto.ca>
2024-12-02 07:09:41 -0500

    git commit -m 'updated the socket connection to be the updated website'

--- app/messages/page.js
+++ app/messages/page.js
@@ -27,7 +27,7 @@ export default function Messages() {
   const uid = user ? user.uid : null;
   
   useEffect(() => {
-    socketRef.current = io('http://localhost:5000', {transports: ['websocket', 'polling'], timeout: 10000}); 
+    socketRef.current = io(/* 'http://localhost:5000', */ 'http://34.0.41.88:5000', {transports: ['websocket', 'polling'], timeout: 10000}); 
     async function getEnrolledCourses() {
       try {
         const response = await fetch(`/api/getEnrolledCourses?uid=${uid}`, {
