573d4e93743eec2171eafe9903e277a916e6521b
Masahisa Sekita <masahisa.sekita@mail.utoronto.ca>
2024-12-02 16:53:52 -0500

    git commit -m 'a thing'

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,21 @@ const nextConfig = {
     async headers() {
       return [
         {
+          module.exports = {
+            async headers() {
+              return [
+                {
+                  source: '/uploads/(.*)', // Match all files in /uploads
+                  headers: [
+                    {
+                      key: 'Cache-Control',
+                      value: 'public, max-age=31536000, immutable', // Set caching for images
+                    },
+                  ],
+                },
+              ];
+            },
+          },
           // Match all routes
           source: '/:path*',
           headers: [
@@ -23,6 +38,21 @@ const nextConfig = {
               key: 'Access-Control-Allow-Credentials',
               value: 'true',
             },
+            module.exports = {
+              async headers() {
+                return [
+                  {
+                    source: '/uploads/(.*)', // Match all files in /uploads
+                    headers: [
+                      {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable', // Set caching for images
+                      },
+                    ],
+                  },
+                ];
+              },
+            }
           ],
         },
       ];
